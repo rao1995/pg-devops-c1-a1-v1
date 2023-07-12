ed7effb28219d7f753e6eb6a044defb88de8a8e3
Karthik Rao V <38070135+rao1995@users.noreply.github.com>
2023-07-12 20:26:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,29 @@
+provider "aws" {
+  region     = "us-east-1"
+}
 
+resource "aws_instance" "karthik-instance-1" {
+  ami           = "${var.ami-mine}"
+  instance_type = "t3.micro"
+  key_name      = "karthik-key"
+
+  tags = {
+    Name = "ANSIBLE"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "${file("karthik-key.pem")}"
+    host        = "${self.public_ip}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo amazon-linux-extras install ansible2 -y",
+      "sudo yum install git -y",
+      "git clone {url} /tmp/git-dir-clone",
+      "ansible-playbook /tmp/git-dir-clone/configurations.yaml"
+    ]
+  }
+}
